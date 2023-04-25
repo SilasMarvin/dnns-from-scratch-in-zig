1d5a2dca2d720181084912fa15c3b28d68bb9c05
jobat <jobat@toolbelt-games.com>
2023-04-25 11:43:23 +0100

    Updated build.zig to work with zig master

--- build.zig
+++ build.zig
@@ -1,14 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{

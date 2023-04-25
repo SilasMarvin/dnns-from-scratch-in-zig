f46d832907be9308d2295f02d5224812226e1e09
jobat <jobat@toolbelt-games.com>
2023-04-25 12:18:23 +0100

    Made NLL not create a new struct per loss function
    Now just uses a comptime parameter in the nll function,
    removing the intermediate struct.

--- src/nll.zig
+++ src/nll.zig
@@ -1,54 +1,46 @@
 const std = @import("std");
+const NLLOuput = struct {
+    loss: []f64,
+    input_grads: []f64,
+    const Self = @This();
 
-pub fn NLL(comptime I: usize) type {
-    const NLLOuput = struct {
-        loss: []f64,
-        input_grads: []f64,
-        const Self = @This();
+    pub fn destruct(self: Self, allocator: *std.mem.Allocator) void {
+        allocator.free(self.loss);
+        allocator.free(self.input_grads);
+    }
+};
 
-        pub fn destruct(self: Self, allocator: *std.mem.Allocator) void {
-            allocator.free(self.loss);
-            allocator.free(self.input_grads);
+pub fn nll(comptime I: usize, inputs: []f64, targets: []u8, allocator: *std.mem.Allocator) !NLLOuput {
+    const batch_size = targets.len;
+    var sum_e = try allocator.alloc(f64, batch_size);
+    defer allocator.free(sum_e);
+    var b: usize = 0;
+    while (b < batch_size) : (b += 1) {
+        var sum: f64 = 0;
+        var i: usize = 0;
+        while (i < I) : (i += 1) {
+            sum += std.math.exp(inputs[b * I + i]);
         }
-    };
+        sum_e[b] = sum;
+    }
 
-    return struct {
-        pub fn nll(inputs: []f64, targets: []u8, allocator: *std.mem.Allocator) !NLLOuput {
-            const batch_size = targets.len;
-            var sum_e = try allocator.alloc(f64, batch_size);
-            defer allocator.free(sum_e);
-            var b: usize = 0;
-            while (b < batch_size) : (b += 1) {
-                var sum: f64 = 0;
-                var i: usize = 0;
-                while (i < I): (i += 1) {
-                    sum += std.math.exp(inputs[b * I + i]);
-                }
-                sum_e[b] = sum;
+    var loss = try allocator.alloc(f64, batch_size);
+    b = 0;
+    while (b < batch_size) : (b += 1) {
+        loss[b] = -1 * std.math.ln(std.math.exp(inputs[b * I + targets[b]]) / sum_e[b]);
+    }
+
+    var input_grads = try allocator.alloc(f64, batch_size * I);
+    b = 0;
+    while (b < batch_size) : (b += 1) {
+        var i: usize = 0;
+        while (i < I) : (i += 1) {
+            input_grads[b * I + i] = std.math.exp(inputs[b * I + i]) / sum_e[b];
+            if (i == targets[b]) {
+                input_grads[b * I + i] -= 1;
             }
-
-            var loss = try allocator.alloc(f64, batch_size);
-            b = 0;
-            while (b < batch_size): (b += 1) {
-                loss[b] = -1 * std.math.ln(std.math.exp(inputs[b * I + targets[b]]) / sum_e[b]);
-            }
-
-            var input_grads = try allocator.alloc(f64, batch_size * I);
-            b = 0;
-            while (b < batch_size): (b += 1) {
-                var i: usize = 0;
-                while (i < I): (i += 1) {
-                    input_grads[b * I + i] = std.math.exp(inputs[b * I + i]) / sum_e[b];
-                    if (i == targets[b]) {
-                        input_grads[b * I + i] -= 1;
-                    }
-                }
-            }
-
-            return NLLOuput {
-                .loss = loss,
-                .input_grads = input_grads 
-            };
         }
-    };
+    }
+
+    return NLLOuput{ .loss = loss, .input_grads = input_grads };
 }
